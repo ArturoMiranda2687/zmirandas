4b257d89593658f30bda34bb5edaf1c840ae7c15
Arturo Miranda <psic.arturomiranda@gmail.com>
2023-08-24 15:03:49 -0600

    2408231503

--- funcion.js
+++ funcion.js
@@ -158,6 +158,91 @@ const danza11 = new Danza({
     esLenceria:true,
     imagen:"./media/prendas/baile/bl11.jpg",
 })
+const danza12 = new Danza({
+    modelo:"Alguno",
+    esLenceria:true,
+    imagen:"./media/prendas/baile/bl12.jpg",
+})
+const danza13 = new Danza({
+    modelo:"Alguno",
+    esLenceria:true,
+    imagen:"./media/prendas/baile/bl13.jpg",
+})
+const danza14 = new Danza({
+    modelo:"Alguno",
+    esLenceria:true,
+    imagen:"./media/prendas/baile/bl14.jpg",
+})
+const danza15 = new Danza({
+    modelo:"Alguno",
+    esLenceria:true,
+    imagen:"./media/prendas/baile/bl15.jpg",
+})
+const danza16 = new Danza({
+    modelo:"Alguno",
+    esLenceria:true,
+    imagen:"./media/prendas/baile/bl16.jpg",
+})
+const danza17 = new Danza({
+    modelo:"Alguno",
+    esLenceria:true,
+    imagen:"./media/prendas/baile/bl17.jpg",
+})
+const danza18 = new Danza({
+    modelo:"Alguno",
+    esLenceria:true,
+    imagen:"./media/prendas/baile/bl18.jpg",
+})
+const danza19 = new Danza({
+    modelo:"Alguno",
+    esLenceria:true,
+    imagen:"./media/prendas/baile/bl19.jpg",
+})
+const danza20 = new Danza({
+    modelo:"Alguno",
+    esLenceria:true,
+    imagen:"./media/prendas/baile/bl20.jpg",
+})
+const danza21 = new Danza({
+    modelo:"Alguno",
+    esLenceria:true,
+    imagen:"./media/prendas/baile/bl21.jpg",
+})
+const danza22 = new Danza({
+    modelo:"Alguno",
+    esLenceria:true,
+    imagen:"./media/prendas/baile/bl22.jpg",
+})
+const danza23 = new Danza({
+    modelo:"Alguno",
+    esLenceria:true,
+    imagen:"./media/prendas/baile/bl23.jpg",
+})
+const danza24 = new Danza({
+    modelo:"Alguno",
+    esLenceria:true,
+    imagen:"./media/prendas/baile/bl24.jpg",
+})
+const danza25 = new Danza({
+    modelo:"Alguno",
+    esLenceria:true,
+    imagen:"./media/prendas/baile/bl25.png",
+})
+const danza26 = new Danza({
+    modelo:"Alguno",
+    esLenceria:true,
+    imagen:"./media/prendas/baile/bl26.jpg",
+})
+const danza27 = new Danza({
+    modelo:"Alguno",
+    esLenceria:true,
+    imagen:"./media/prendas/baile/bl27.jpg",
+})
+const danza28 = new Danza({
+    modelo:"Alguno",
+    esLenceria:true,
+    imagen:"./media/prendas/baile/bl28.jpg",
+})
 
 
 prodcatal.innerHTML += `<h4>${m001.modelo}</h4><div class="galeria"><div class="flechai"></div><div class="flechad"></div><img src="${m001.imagen}" alt=""></div><p>Tallas disponibles: ${m001.talla}</p><p>Tela: ${m001.tela}</p><p>${m001.descripcion}</p>`
@@ -177,3 +262,21 @@ prodcatadanza.innerHTML += `<h4>${danza8.modelo}</h4><div class="galeria"><div c
 prodcatadanza.innerHTML += `<h4>${danza9.modelo}</h4><div class="galeria"><div class="flechai"></div><div class="flechad"></div><img src="${danza9.imagen}" alt=""></div><p>Tallas disponibles: ${danza9.talla}</p><p>Tela: ${danza9.tela}</p><p>${danza9.descripcion}</p>`
 prodcatadanza.innerHTML += `<h4>${danza10.modelo}</h4><div class="galeria"><div class="flechai"></div><div class="flechad"></div><img src="${danza10.imagen}" alt=""></div><p>Tallas disponibles: ${danza10.talla}</p><p>Tela: ${danza10.tela}</p><p>${danza10.descripcion}</p>`
 prodcatadanza.innerHTML += `<h4>${danza11.modelo}</h4><div class="galeria"><div class="flechai"></div><div class="flechad"></div><img src="${danza11.imagen}" alt=""></div><p>Tallas disponibles: ${danza11.talla}</p><p>Tela: ${danza11.tela}</p><p>${danza11.descripcion}</p>`
+prodcatadanza.innerHTML += `<h4>${danza12.modelo}</h4><div class="galeria"><div class="flechai"></div><div class="flechad"></div><img src="${danza12.imagen}" alt=""></div><p>Tallas disponibles: ${danza12.talla}</p><p>Tela: ${danza12.tela}</p><p>${danza12.descripcion}</p>`
+prodcatadanza.innerHTML += `<h4>${danza13.modelo}</h4><div class="galeria"><div class="flechai"></div><div class="flechad"></div><img src="${danza13.imagen}" alt=""></div><p>Tallas disponibles: ${danza13.talla}</p><p>Tela: ${danza13.tela}</p><p>${danza13.descripcion}</p>`
+prodcatadanza.innerHTML += `<h4>${danza14.modelo}</h4><div class="galeria"><div class="flechai"></div><div class="flechad"></div><img src="${danza14.imagen}" alt=""></div><p>Tallas disponibles: ${danza14.talla}</p><p>Tela: ${danza14.tela}</p><p>${danza14.descripcion}</p>`
+prodcatadanza.innerHTML += `<h4>${danza15.modelo}</h4><div class="galeria"><div class="flechai"></div><div class="flechad"></div><img src="${danza15.imagen}" alt=""></div><p>Tallas disponibles: ${danza15.talla}</p><p>Tela: ${danza15.tela}</p><p>${danza15.descripcion}</p>`
+prodcatadanza.innerHTML += `<h4>${danza16.modelo}</h4><div class="galeria"><div class="flechai"></div><div class="flechad"></div><img src="${danza16.imagen}" alt=""></div><p>Tallas disponibles: ${danza16.talla}</p><p>Tela: ${danza16.tela}</p><p>${danza16.descripcion}</p>`
+prodcatadanza.innerHTML += `<h4>${danza17.modelo}</h4><div class="galeria"><div class="flechai"></div><div class="flechad"></div><img src="${danza17.imagen}" alt=""></div><p>Tallas disponibles: ${danza17.talla}</p><p>Tela: ${danza17.tela}</p><p>${danza17.descripcion}</p>`
+prodcatadanza.innerHTML += `<h4>${danza18.modelo}</h4><div class="galeria"><div class="flechai"></div><div class="flechad"></div><img src="${danza18.imagen}" alt=""></div><p>Tallas disponibles: ${danza18.talla}</p><p>Tela: ${danza18.tela}</p><p>${danza18.descripcion}</p>`
+prodcatadanza.innerHTML += `<h4>${danza19.modelo}</h4><div class="galeria"><div class="flechai"></div><div class="flechad"></div><img src="${danza19.imagen}" alt=""></div><p>Tallas disponibles: ${danza19.talla}</p><p>Tela: ${danza19.tela}</p><p>${danza19.descripcion}</p>`
+prodcatadanza.innerHTML += `<h4>${danza20.modelo}</h4><div class="galeria"><div class="flechai"></div><div class="flechad"></div><img src="${danza20.imagen}" alt=""></div><p>Tallas disponibles: ${danza20.talla}</p><p>Tela: ${danza20.tela}</p><p>${danza20.descripcion}</p>`
+prodcatadanza.innerHTML += `<h4>${danza21.modelo}</h4><div class="galeria"><div class="flechai"></div><div class="flechad"></div><img src="${danza21.imagen}" alt=""></div><p>Tallas disponibles: ${danza21.talla}</p><p>Tela: ${danza21.tela}</p><p>${danza21.descripcion}</p>`
+prodcatadanza.innerHTML += `<h4>${danza22.modelo}</h4><div class="galeria"><div class="flechai"></div><div class="flechad"></div><img src="${danza22.imagen}" alt=""></div><p>Tallas disponibles: ${danza22.talla}</p><p>Tela: ${danza22.tela}</p><p>${danza22.descripcion}</p>`
+prodcatadanza.innerHTML += `<h4>${danza23.modelo}</h4><div class="galeria"><div class="flechai"></div><div class="flechad"></div><img src="${danza23.imagen}" alt=""></div><p>Tallas disponibles: ${danza23.talla}</p><p>Tela: ${danza23.tela}</p><p>${danza23.descripcion}</p>`
+prodcatadanza.innerHTML += `<h4>${danza24.modelo}</h4><div class="galeria"><div class="flechai"></div><div class="flechad"></div><img src="${danza24.imagen}" alt=""></div><p>Tallas disponibles: ${danza24.talla}</p><p>Tela: ${danza24.tela}</p><p>${danza24.descripcion}</p>`
+prodcatadanza.innerHTML += `<h4>${danza25.modelo}</h4><div class="galeria"><div class="flechai"></div><div class="flechad"></div><img src="${danza25.imagen}" alt=""></div><p>Tallas disponibles: ${danza25.talla}</p><p>Tela: ${danza25.tela}</p><p>${danza25.descripcion}</p>`
+prodcatadanza.innerHTML += `<h4>${danza26.modelo}</h4><div class="galeria"><div class="flechai"></div><div class="flechad"></div><img src="${danza26.imagen}" alt=""></div><p>Tallas disponibles: ${danza26.talla}</p><p>Tela: ${danza26.tela}</p><p>${danza26.descripcion}</p>`
+prodcatadanza.innerHTML += `<h4>${danza27.modelo}</h4><div class="galeria"><div class="flechai"></div><div class="flechad"></div><img src="${danza27.imagen}" alt=""></div><p>Tallas disponibles: ${danza27.talla}</p><p>Tela: ${danza27.tela}</p><p>${danza27.descripcion}</p>`
+prodcatadanza.innerHTML += `<h4>${danza28.modelo}</h4><div class="galeria"><div class="flechai"></div><div class="flechad"></div><img src="${danza28.imagen}" alt=""></div><p>Tallas disponibles: ${danza28.talla}</p><p>Tela: ${danza28.tela}</p><p>${danza28.descripcion}</p>`
+
