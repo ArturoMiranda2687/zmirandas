b3aa63f2fb61987c06592e3fc945d3f75ffe5d8b
Arturo Miranda <psic.arturomiranda@gmail.com>
2023-10-25 15:19:31 -0600

    2510231519

--- recursos/catdanza.js
+++ recursos/catdanza.js
@@ -1,4 +1,4 @@
-const prodcatadanza = document.querySelector('.catalogodanza')
+const prodcatadanza = document.querySelector('.danza')
 
 
 class Danza extends Producto{
@@ -138,225 +138,27 @@ const danza24 = new Danza({
     imagen2:"./media/prendas/baile/bl38.png",
 })
 
+var productos = [
+    danza1,danza2,danza3,danza4,danza5,danza6,danza7,danza8,danza9,danza10,danza11,danza12,danza13,danza14,danza15,danza16,/*danza17,danza18*/,danza19,danza20,danza21,danza22,danza23,danza24
+]
 
 
-prodcatadanza.innerHTML += `<h4>${danza1.modelo}</h4><div class="galeria"><img src="${danza1.imagen}" alt=""><img src="${danza1.imagen2}" alt=""><img src="${danza1.imagen3}" alt=""></div><div class="contenedorVistaPrevia"><img src="${danza1.imagen2}" alt=""><img src="${danza1.imagen3}" alt=""></div><p>Tallas disponibles: ${danza1.talla}</p><p>Tela: ${danza1.tela}</p><p>${danza1.descripcion}</p>`
-prodcatadanza.innerHTML += `<h4>
-${danza2.modelo}</h4><div class="galeria"><img src="
-${danza2.imagen}" alt=""></div><div class="contenedorVistaPrevia"><img src="
-${danza2.imagen2}" alt=""><img src="
-${danza2.imagen3}" alt=""></div><p>Tallas disponibles: 
-${danza2.talla}</p><p>Tela: 
-${danza2.tela}</p><p>
-${danza2.descripcion}</p>`
+productos.forEach(function(producto)
+{
+    var productoElement = document.createElement("div")
 
-prodcatadanza.innerHTML += `<h4>
-${danza3.modelo}</h4><div class="galeria"><img src="
-${danza3.imagen}" alt=""></div><div class="contenedorVistaPrevia"><img src="
-${danza3.imagen2}" alt=""><img src="
-${danza3.imagen3}" alt=""></div><p>Tallas disponibles: 
-${danza3.talla}</p><p>Tela: 
-${danza3.tela}</p><p>
-${danza3.descripcion}</p>`
+    productoElement.innerHTML = `<h4>
+    ${producto.modelo}</h4><div class="galeria"><img src="
+    ${producto.imagen}" alt=""><img src="
+    ${producto.imagen2}" alt=""><img src="
+    ${producto.imagen3}" alt=""><img src="
+    ${producto.imagen4}" alt=""></div><div class="contenedorVistaPrevia"><img src="
+    ${producto.imagen2}" alt=""><img src="
+    ${producto.imagen3}" alt=""><img src="
+    ${producto.imagen4}" alt=""></div><div class: "parrafos"> <p>Tallas disponibles: 
+    ${producto.talla}</p><p>Tela: 
+    ${producto.tela}</p><p>
+    ${producto.descripcion}</p></div>`
 
-prodcatadanza.innerHTML += `<h4>
-${danza4.modelo}</h4><div class="galeria"><img src="
-${danza4.imagen}" alt=""></div><div class="contenedorVistaPrevia"><img src="
-${danza4.imagen2}" alt=""><img src="
-${danza4.imagen3}" alt=""></div><p>Tallas disponibles: 
-${danza4.talla}</p><p>Tela: 
-${danza4.tela}</p><p>
-${danza4.descripcion}</p>`
-
-prodcatadanza.innerHTML += `<h4>
-${danza5.modelo}</h4><div class="galeria"><img src="
-${danza5.imagen}" alt=""></div><div class="contenedorVistaPrevia"><img src="
-${danza5.imagen2}" alt=""><img src="
-${danza5.imagen3}" alt=""></div><p>Tallas disponibles: 
-${danza5.talla}</p><p>Tela: 
-${danza5.tela}</p><p>
-${danza5.descripcion}</p>`
-
-prodcatadanza.innerHTML += `<h4>
-${danza6.modelo}</h4><div class="galeria"><img src="
-${danza6.imagen}" alt=""></div><div class="contenedorVistaPrevia"><img src="
-${danza6.imagen2}" alt=""><img src="
-${danza6.imagen3}" alt=""></div><p>Tallas disponibles: 
-${danza6.talla}</p><p>Tela: 
-${danza6.tela}</p><p>
-${danza6.descripcion}</p>`
-
-prodcatadanza.innerHTML += `<h4>
-${danza7.modelo}</h4><div class="galeria"><img src="
-${danza7.imagen}" alt=""><img src="
-${danza7.imagen2}" alt=""><img src="
-${danza7.imagen3}" alt=""></div><div class="contenedorVistaPrevia"><img src="
-${danza7.imagen2}" alt=""><img src="
-${danza7.imagen3}" alt=""></div><p>Tallas disponibles: 
-${danza7.talla}</p><p>Tela: 
-${danza7.tela}</p><p>
-${danza7.descripcion}</p>`
-
-prodcatadanza.innerHTML += `<h4>
-${danza8.modelo}</h4><div class="galeria"><img src="
-${danza8.imagen}" alt=""></div><div class="contenedorVistaPrevia"><img src="
-${danza8.imagen2}" alt=""><img src="
-${danza8.imagen3}" alt=""></div><p>Tallas disponibles: 
-${danza8.talla}</p><p>Tela: 
-${danza8.tela}</p><p>
-${danza8.descripcion}</p>`
-
-prodcatadanza.innerHTML += `<h4>
-${danza9.modelo}</h4><div class="galeria"><img src="
-${danza9.imagen}" alt=""></div><div class="contenedorVistaPrevia"><img src="
-${danza9.imagen2}" alt=""><img src="
-${danza9.imagen3}" alt=""></div><p>Tallas disponibles: 
-${danza9.talla}</p><p>Tela: 
-${danza9.tela}</p><p>
-${danza9.descripcion}</p>`
-
-prodcatadanza.innerHTML += `<h4>
-${danza10.modelo}</h4><div class="galeria"><img src="
-${danza10.imagen}" alt=""></div><div class="contenedorVistaPrevia"><img src="
-${danza10.imagen2}" alt=""><img src="
-${danza10.imagen3}" alt=""></div><p>Tallas disponibles: 
-${danza10.talla}</p><p>Tela: 
-${danza10.tela}</p><p>
-${danza10.descripcion}</p>`
-
-prodcatadanza.innerHTML += `<h4>
-${danza11.modelo}</h4><div class="galeria"><img src="
-${danza11.imagen}" alt=""><img src="
-${danza11.imagen2}" alt=""><img src="
-${danza11.imagen3}" alt=""><img src="
-${danza11.imagen4}" alt=""><img src="
-${danza11.imagen5}" alt=""></div><div class="contenedorVistaPrevia"><img src="
-${danza11.imagen2}" alt=""><img src="
-${danza11.imagen3}" alt=""><img src="
-${danza11.imagen4}" alt=""><img src="
-${danza11.imagen5}" alt=""></div><p>Tallas disponibles: 
-${danza11.talla}</p><p>Tela: 
-${danza11.tela}</p><p>
-${danza11.descripcion}</p>`
-prodcatadanza.innerHTML += `<h4>
-${danza12.modelo}</h4><div class="galeria"><img src="
-${danza12.imagen}" alt=""></div><div class="contenedorVistaPrevia"><img src="
-${danza12.imagen2}" alt=""><img src="
-${danza12.imagen3}" alt=""></div><p>Tallas disponibles: 
-${danza12.talla}</p><p>Tela: 
-${danza12.tela}</p><p>
-${danza12.descripcion}</p>`
-
-prodcatadanza.innerHTML += `<h4>
-${danza13.modelo}</h4><div class="galeria"><img src="
-${danza13.imagen}" alt=""><img src="
-${danza13.imagen2}" alt=""><img src="
-${danza13.imagen3}" alt=""><img src="
-${danza13.imagen4}" alt=""></div><div class="contenedorVistaPrevia"><img src="
-${danza13.imagen2}" alt=""><img src="
-${danza13.imagen3}" alt=""><img src="
-${danza13.imagen4}" alt=""></div><p>Tallas disponibles: 
-${danza13.talla}</p><p>Tela: 
-${danza13.tela}</p><p>
-${danza13.descripcion}</p>`
-
-prodcatadanza.innerHTML += `<h4>
-${danza14.modelo}</h4><div class="galeria"><img src="
-${danza14.imagen}" alt=""></div><div class="contenedorVistaPrevia"><img src="
-${danza14.imagen2}" alt=""><img src="
-${danza14.imagen3}" alt=""></div><p>Tallas disponibles: 
-${danza14.talla}</p><p>Tela: 
-${danza14.tela}</p><p>
-${danza14.descripcion}</p>`
-
-prodcatadanza.innerHTML += `<h4>
-${danza15.modelo}</h4><div class="galeria"><img src="
-${danza15.imagen}" alt=""></div><div class="contenedorVistaPrevia"><img src="
-${danza15.imagen2}" alt=""><img src="
-${danza15.imagen3}" alt=""></div><p>Tallas disponibles: 
-${danza15.talla}</p><p>Tela: 
-${danza15.tela}</p><p>
-${danza15.descripcion}</p>`
-
-prodcatadanza.innerHTML += `<h4>
-${danza16.modelo}</h4><div class="galeria"><img src="
-${danza16.imagen}" alt=""></div><div class="contenedorVistaPrevia"><img src="
-${danza16.imagen2}" alt=""><img src="
-${danza16.imagen3}" alt=""></div><p>Tallas disponibles: 
-${danza16.talla}</p><p>Tela: 
-${danza16.tela}</p><p>
-${danza16.descripcion}</p>`
-//
-//prodcatadanza.innerHTML += `<h4>
-//${danza17.modelo}</h4><div class="galeria"><img src="
-//${danza17.imagen}" alt=""></div><div class="contenedorVistaPrevia"><img src="
-//${danza17.imagen2}" alt=""><img src="
-//${danza17.imagen3}" alt=""></div><p>Tallas disponibles: 
-//${danza17.talla}</p><p>Tela: 
-//${danza17.tela}</p><p>
-//${danza17.descripcion}</p>`
-//
-//prodcatadanza.innerHTML += `<h4>
-//${danza18.modelo}</h4><div class="galeria"><img src="
-//${danza18.imagen}" alt=""></div><div class="contenedorVistaPrevia"><img src="
-//${danza18.imagen2}" alt=""><img src="
-//${danza18.imagen3}" alt=""></div><p>Tallas disponibles: 
-//${danza18.talla}</p><p>Tela: 
-//${danza18.tela}</p><p>
-//${danza18.descripcion}</p>`
-
-prodcatadanza.innerHTML += `<h4>
-${danza19.modelo}</h4><div class="galeria"><img src="
-${danza19.imagen}" alt=""></div><div class="contenedorVistaPrevia"><img src="
-${danza19.imagen2}" alt=""><img src="
-${danza19.imagen3}" alt=""></div><p>Tallas disponibles: 
-${danza19.talla}</p><p>Tela: 
-${danza19.tela}</p><p>
-${danza19.descripcion}</p>`
-
-prodcatadanza.innerHTML += `<h4>
-${danza20.modelo}</h4><div class="galeria"><img src="
-${danza20.imagen}" alt=""></div><div class="contenedorVistaPrevia"><img src="
-${danza20.imagen2}" alt=""><img src="
-${danza20.imagen3}" alt=""></div><p>Tallas disponibles: 
-${danza20.talla}</p><p>Tela: 
-${danza20.tela}</p><p>
-${danza20.descripcion}</p>`
-
-prodcatadanza.innerHTML += `<h4>
-${danza21.modelo}</h4><div class="galeria"><img src="
-${danza21.imagen}" alt=""></div><div class="contenedorVistaPrevia"><img src="
-${danza21.imagen2}" alt=""><img src="
-${danza21.imagen3}" alt=""></div><p>Tallas disponibles: 
-${danza21.talla}</p><p>Tela: 
-${danza21.tela}</p><p>
-${danza21.descripcion}</p>`
-
-prodcatadanza.innerHTML += `<h4>
-${danza22.modelo}</h4><div class="galeria"><img src="
-${danza22.imagen}" alt=""><img src="
-${danza22.imagen2}" alt=""></div><div class="contenedorVistaPrevia"><img src="
-${danza22.imagen2}" alt=""><img src="
-${danza22.imagen3}" alt=""></div><p>Tallas disponibles: 
-${danza22.talla}</p><p>Tela: 
-${danza22.tela}</p><p>
-${danza22.descripcion}</p>`
-
-prodcatadanza.innerHTML += `<h4>
-${danza23.modelo}</h4><div class="galeria"><img src="
-${danza23.imagen}" alt=""></div><div class="contenedorVistaPrevia"><img src="
-${danza23.imagen2}" alt=""><img src="
-${danza23.imagen3}" alt=""></div><p>Tallas disponibles: 
-${danza23.talla}</p><p>Tela: 
-${danza23.tela}</p><p>
-${danza23.descripcion}</p>`
-
-prodcatadanza.innerHTML += `<h4>
-${danza24.modelo}</h4><div class="galeria"><img src="
-${danza24.imagen}" alt=""><img src="
-${danza24.imagen2}" alt=""></div><div class="contenedorVistaPrevia"><img src="
-${danza24.imagen2}" alt=""><img src="
-${danza24.imagen3}" alt=""></div><p>Tallas disponibles: 
-${danza24.talla}</p><p>Tela: 
-${danza24.tela}</p><p>
-${danza24.descripcion}</p>`
+    prodcatadanza.appendChild(productoElement)
+})
--- recursos/funcion.js
+++ recursos/funcion.js
@@ -4,7 +4,7 @@ const bienvenida = document.querySelector('.bienvenida')
 const saludos = document.querySelector('.saludos')
 const footer = document.querySelector('footer')
 
-menunav.innerHTML +=`<nav><ul> <li class="inactivo"><a href="./index.html">Inicio</a></li>   <li><a href="./lenceria.html">Lenceria</a></li> <li><a href="./danza.html">Danza</a></li> <li><a href="./elastano.html">Deportiva</a></li>  <li><a href="">Pijamas</a></li>    <li><a href="./trajesBaño.html">Traje de baño</a></li></ul></nav>`
+menunav.innerHTML +=`<nav><ul> <li class="inactivo"><a href="./index.html">Inicio</a></li>   <li><a href="./lenceria.html">Lenceria</a></li> <li><a href="./danza.html">Danza</a></li> <li><a href="#elastano">Deportiva</a></li>  <li><a href="">Pijamas</a></li>    <li><a href="./trajesBaño.html">Traje de baño</a></li></ul></nav>`
 cabecera.innerHTML += `<h1>Mir and Da</h1>`
 footer.innerHTML += `<p>Mir & Da a tus órdenes</p><p>55245874 25</p>`
 
